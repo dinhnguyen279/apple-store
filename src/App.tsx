016d621c3282ea2b67d1432718b2740c279ebabb
Nguyễn <tdinhnguyen279@gmail.com>
2022-11-17 07:30:56 +0700

    feat: :memo: Update format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-// import "./App.css";
-import Header from "./components/Header";
-
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import "./App.css";
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
