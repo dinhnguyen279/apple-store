0a322bb3625337fbb49ed660b1e526b732f3c790
Nguyễn <tdinhnguyen279@gmail.com>
2022-11-19 14:16:08 +0700

    feat: :lipstick: responsive navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "../../assets/images/logomain.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
   faBagShopping,
   faSearch,
+  faRefresh,
 } from "@fortawesome/free-solid-svg-icons";
+import "./header.css";
 const Header = () => {
+  const navRef = useRef<HTMLUListElement | null>(null);
+  const [show, setShow] = useState<boolean>(true);
+  const showNav = () => {
+    navRef.current?.classList.toggle("responsive-nav");
+    setShow(!show);
+    console.log("click");
+  };
+
   return (
-    <div className="bg-[#323232] text-zinc-300 py-2 text-xs">
-      <div className="flex justify-center items-center gap-10 container mx-auto">
-        <button className=" lg:hidden flex justify-start">
-          <FontAwesomeIcon icon={faBars} />
+    <div className="bg-black text-zinc-300 py-2 text-xs ">
+      <div className="flex justify-center items-center gap-10 container mx-auto nav-main">
+        <button
+          className="lg:hidden flex justify-start btn-bar"
+          onClick={showNav}
+        >
+          {show ? (
+            <FontAwesomeIcon icon={faBars} />
+          ) : (
+            <FontAwesomeIcon icon={faRefresh} />
+          )}
         </button>
         <div className=" lg:m-0 m-auto">
           <img src={logo} alt="logo" className="h-6 w-6" />
         </div>
-        <div>
-          <ul className="hidden justify-evenly items-center gap-10 lg:flex">
+        <div className="">
+          <ul
+            className="flex  justify-evenly items-center gap-10 w-full "
+            ref={navRef}
+          >
             <li>
               <a href="/">Store</a>
             </li>
@@ -48,7 +68,8 @@ const Header = () => {
             <li>
               <a href="/">Support</a>
             </li>
-            <FontAwesomeIcon icon={faSearch} />
+
+            <FontAwesomeIcon icon={faSearch} className="lg:block hidden" />
           </ul>
         </div>
         <button type="submit" className="text-sm">
